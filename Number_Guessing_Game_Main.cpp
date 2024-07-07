c0008bda139062faf97b2acd9c1c7e4f6a990189
Tripp Hopkins <tripphopkins13245@gmail.com>
2024-07-06 23:12:20 -0400

    Added PB code

--- Number_Guessing_Game_Main.cpp
+++ Number_Guessing_Game_Main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <ctime>
 #include <cmath>
+#include <fstream>
 
 int main(){
 
@@ -14,6 +15,7 @@ int main(){
     std::cout << "******* Number guessing game *******\n";
 
     do{
+        bool gameEnd = false;
         std::cout << "enter a guess between 1 and 100\n";
         std::cin >> guess;
         tries++;
@@ -34,9 +36,55 @@ int main(){
         std::cout << "Congrats, Thats the Correct Number!\n";
         std::cout << "Tries: " << tries << '\n';
     }
+        std::string fileContent;
+
+
+        std::ifstream iscoreFileStream;
+        std::ofstream oscoreFileStream;
+        std::string fileName = "high_score_keeper.txt";
+
+        iscoreFileStream.open(fileName);
+        iscoreFileStream >> fileContent;
+        iscoreFileStream.close();
         
-        
+        if(fileContent != ""){
+            int highScore = std::stoi (fileContent);
+
+            if(tries < highScore){
+                std::cout << "NEW HIGH SCORE! YIPEE\n";
+                oscoreFileStream.open(fileName);
+                oscoreFileStream << tries;
+                oscoreFileStream.close();
+            }
+        }
+        else{
+            oscoreFileStream.open(fileName);
+            oscoreFileStream << tries;
+            oscoreFileStream.close();
+        }
+            
+
+/*
+        iscoreFileStream.open ("high_score_keeper.txt");
+        scoreFileStream >> fileContent;
+        if(fileContent != ""){
+            int highScore = std::stoi (fileContent);
+            if(highScore > tries){
+                std::cout << "NEW HIGH SCORE!\n";
+                scoreFileStream << tries;
+            }
+        }
+        else{
+            scoreFileStream << tries;
+        }
+
+*/
+
+
+       
+       
         std::cout << "*************************************";
+        bool gameEnd = true;
 
 
     return 0;
